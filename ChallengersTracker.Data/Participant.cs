3e567ee409412133a96fddeb7fac5b92d2f0f3bc
Theodor Bellash <theobellash@gmail.com>
2022-01-07 12:15:16 +0100

    Installed Tweetinvi, twitter confirmation pending

---
+++ ChallengersTracker.Data/Participant.cs
@@ -0,0 +1,12 @@
+﻿namespace ChallengersTracker.Data
+{
+    public class Participant
+    {
+        public string ParticipantId { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Twitter { get; set; } = "";
+        public string Github { get; set; } = "";
+        public string ProjectName { get; set; } = "";
+        public string ProjectDescription { get; set; } = "";
+    }
+}
--- ChallengersTracker/Program.cs
+++ ChallengersTracker/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tweetinvi;
+
+var tweetTexts = new List<string>();
+tweetTexts.Add("I just started a new project on GitHub. It will allow you to solve the sudoku puzzle. Check it out at: https://github.com/Dimon24021993/Sudoku-solver \n#csharpchallenge \n@IAmTimCorey");
+tweetTexts.Add("I just started a new project on GitHub. It will allow you to calculate your mortgage payments with just a few basic values. Total mortgage cost, Monthly payment, as well as how a prepayment will affect your monthly payments.\nhttps://github.com/mikevankuik/MortgageCalculator\n#csharpchallenge @IAmTimCorey");
+
+
+var userClient = new TwitterClient("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+var keywords = new List<string>() { "#csharpchallenge", "@IAmTimCorey" };
+
+
+var searchResponse = await userClient.SearchV2.SearchTweetsAsync(keywords[0]);
+var tweets = searchResponse.Tweets.ToList();
+
+Console.WriteLine("Welcome to challengers trackers!");
+foreach (var tweet in tweets)
+{
+    Console.WriteLine($"{tweet.AuthorId} {tweet.CreatedAt.ToString("yyyy-MM-dd hh:mm")} : {tweet.Text}");
+}
+Console.ReadLine();
